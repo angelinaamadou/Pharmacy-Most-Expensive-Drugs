bf05d67a28c1cb4c567c8504b00b1420b98091f7
Angelina Amadou <40861126+angelinaamadou@users.noreply.github.com>
2018-07-15 18:42:22 -0400

    Create run_tests.sh

---
+++ insight_testsuite/run_tests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python ../src/pharmacy_top_drug.py ./tests/test_1/input/itcont.txt ./tests/test_1/output/top_cost_drug.txt
+python ../src/pharmacy_top_drug.py ./tests/test_2/input/test2.txt ./tests/test_2/output/top_cost_drug.txt
